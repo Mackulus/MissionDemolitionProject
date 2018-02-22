7ca6edd7a9d68c0e3d09c3d439f3a71f35a8f009
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-02-21 22:10:01 -0600

    Added Background Castles
    - Added background castle prefabs
    - Added a script to randomly generate one of three castles and place it on the scene.
    - Added an additional camera to add depth and make the other castle appear to be "behind" the scene when in fact it is just an illusion from the camera.
    
    Issues:
     - Castle slides with camera and am unsure if that is how I would like it to be or if it should stay in place.
     - Unsure of how dynamic the castle generation needs to be.

---
+++ Mission Demolition Prototype/Assets/__Scripts/CastleGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleGeneration : MonoBehaviour {
+
+	public GameObject prefabCastle1;
+	public GameObject prefabCastle2;
+	public GameObject prefabCastle3;
+	public Vector3 castlePosition;
+
+	private GameObject castle;
+	void Start () {
+		PlaceCastle();
+	}
+
+	private void PlaceCastle()
+	{
+		int random = Random.Range(1, 3);
+		if (random == 1)
+			castle = Instantiate(prefabCastle1);
+		else if (random == 2)
+			castle = Instantiate(prefabCastle2);
+		else if (random == 3)
+			castle = Instantiate(prefabCastle3);
+		castle.transform.position = castlePosition;
+	}
+}
