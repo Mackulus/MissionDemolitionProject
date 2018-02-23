282bf64fb8702b4ae1e0b101c5a3c335acc406fc
Mack Peters <mpeters@harding.edu>
2018-02-22 21:16:35 -0600

    Added a main menu
    -Title doesn't have to stay
    -Music has an awkward pause at the end before it loops and I tried to get garageband to cooperate but it is being difficult

---
+++ Mission Demolition Prototype/Assets/__Scripts/MushroomPanelActivator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MushroomPanelActivator : MonoBehaviour {
+
+	public GameObject mushroomPanel;
+
+	public void OnMouseEnter()
+	{
+		mushroomPanel.SetActive(true);
+	}
+
+	public void OnMouseExit()
+	{
+		mushroomPanel.SetActive(false);
+	}
+}
