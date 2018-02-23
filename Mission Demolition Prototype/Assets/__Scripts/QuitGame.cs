bfc6328769a3fc8a9599bef029b5ddcc6de111b8
Mack Peters <mpeters@harding.edu>
2018-02-22 21:30:56 -0600

    Added quit game script
    -Only works for a built game, but will exit the game

---
+++ Mission Demolition Prototype/Assets/__Scripts/QuitGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+	public void Quit() {
+		//This only works in a built game, but it works
+		Application.Quit();
+	}
+}
