bc098018967a57b448adda67efccdf5e5c2c3aee
Mack Peters <mpeters@harding.edu>
2018-02-25 17:30:35 -0600

    Added level select menu
    -Mario goes down a tube when a level is selected
    -Navigated to by pressing the play button and exited when pressing the back button
    
    TO DO:
    -Add some kind of panel that goes over top to say if the level has been completed

---
+++ Mission Demolition Prototype/Assets/__Scripts/MarioLevelSelectController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class MarioLevelSelectController : MonoBehaviour {
+
+	public GameObject mario;
+
+	private bool clicked = false;
+
+	public void OnMouseEnter()
+	{
+		print("Here");
+		mario.SetActive(true);
+	}
+
+	public void OnMouseExit()
+	{
+		if (!clicked)
+		{
+			print("There");
+			mario.SetActive(false);
+		}
+	}
+
+	public void OnMouseUp()
+	{
+		clicked = true;
+		Animator anim = mario.GetComponent<Animator>();
+		anim.Play("Mario Down The Tube");
+		mario.GetComponent<AudioSource>().Play();
+		Invoke("CallLoadScene", 1f);
+	}
+
+	public void CallLoadScene()
+	{
+		string level = GetComponentInChildren<Text>().text;
+		int levelNumber = Int32.Parse(level) - 1;
+		SceneManager.LoadScene("_Scene_"+levelNumber.ToString());
+	}
+}
