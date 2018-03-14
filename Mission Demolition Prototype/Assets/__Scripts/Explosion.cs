79572e5299ed11c6030efef7c8cbb46eb44c3225
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-14 16:43:46 -0500

    Weird bomb stuff
    Made bombs projectiles needs work

--- Mission Demolition Prototype/Assets/__Scripts/Explosion.cs
+++ Mission Demolition Prototype/Assets/__Scripts/Explosion.cs
@@ -10,22 +10,32 @@ public class Explosion : MonoBehaviour
 	public float radius = 5.0F;
 	public float power = 10.0F;
 
-	private void OnCollisionEnter()
+	private void OnCollisionEnter(Collision collision)
 	{
-		bomb = this.gameObject;
-		bomb.GetComponentInParent<AudioSource>().Play();
-		Vector3 explosionPos = transform.position;
-		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
-		foreach (Collider hit in colliders)
-		{
-			Rigidbody rb = hit.GetComponent<Rigidbody>();
+        print(collision.collider.name);
+        if (collision.collider.name != "Ground" && collision.collider.name != "CastleGround"
+            && collision.collider.name != "Mario" && collision.collider.tag != "Dummy Projectile" 
+            && collision.collider.tag != "Projectile")
+        {
+            print("collision");
+            bomb = this.gameObject;
+            //bomb.GetComponentInParent<AudioSource>().Play();
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            print(colliders);
+            foreach (Collider hit in colliders)
+            {
+                print(hit);
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                print(rb);
+                if (rb != null)
+                {
+                    print("explode");
+                    rb.AddExplosionForce(power, explosionPos, radius, 3.0F, ForceMode.Impulse);
+                }
+            }
 
-			if (rb != null)
-			{
-				rb.AddExplosionForce(power, explosionPos, radius, 3.0F, ForceMode.Impulse);
-			}
-		}
-
-		bomb.SetActive(false);
+            bomb.SetActive(false);
+        }
 	}
 }
