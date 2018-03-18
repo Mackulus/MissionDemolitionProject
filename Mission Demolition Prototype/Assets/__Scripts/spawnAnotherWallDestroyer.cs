06659ad7975b0067570de5e2a08c2f227bcfacc5
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-03-18 18:36:15 -0500

    Still working on level 5

---
+++ Mission Demolition Prototype/Assets/__Scripts/spawnAnotherWallDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnAnotherWallDestroyer : MonoBehaviour {
+
+    public GameObject wallDestroyer;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = gameObject.transform.position;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Projectile")
+        {
+            Invoke("createNewDestroyer", 1f);
+        }
+    }
+
+    private void createNewDestroyer()
+    {
+        Instantiate(wallDestroyer).transform.position = startPos;
+    }
+}
